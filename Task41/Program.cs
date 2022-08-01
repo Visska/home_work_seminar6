fc55c8e78121a77e9b76fa73d206416c59a45bea
Visska <strannica.ev@gmail.com>
2022-08-01 14:41:09 +0300

    add Task41, Task43

---
+++ Task41/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+Console.Clear();
+Console.WriteLine("Укажите сколько чисел вы хотите ввести (число М): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите нужные числа: ");
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Введённых вами чисел больше 0 -> {count}.");
---
+++ Task43/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+*/
+
+Console.Clear();
+Console.WriteLine("Для уравнения y = k1 * x + b1 : ");
+Console.WriteLine("Введите значение b1...");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1...");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Для уравнения y = k2 * x + b2 : ");
+Console.WriteLine("Введите значение b2...");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2...");
+double k2 = Convert.ToDouble(Console.ReadLine());
+LineIntersection(b1, k1, b2, k2);
+
+void LineIntersection (double b1, double a1, double b2, double a2)
+{
+    if (a1 == a2 && b1==b2) 
+    {
+        Console.WriteLine("Прямые совпадают.");
+        return;
+    }
+
+    if (a1 == a2)
+    {
+        Console.WriteLine("Решения нет.");
+        return;
+    }
+
+    double x = (b2 - b1) / (a1 - a2);
+    double y = a1 * x + b1;
+    Console.WriteLine($"Точка пересечения: ({x};{y})");
+    
+}
